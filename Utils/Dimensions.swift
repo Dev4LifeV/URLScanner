3d792c577624613a767de9af6296878318e84901
Victor <vctudo.27@gmail.com>
2023-02-01 14:48:10 -0300

    🧑‍💻 extensions for auto layout code improvement

---
+++ Utils/Dimensions.swift
@@ -0,0 +1,53 @@
+//
+//  Size.swift
+//  URLScanner
+//
+//  Created by Victor Proppe on 26/01/23.
+//
+
+import UIKit
+
+extension Array where Element: NSLayoutConstraint {
+    func activate() {
+        NSLayoutConstraint.activate(self)
+    }
+}
+
+extension UIView {
+    
+    func enableAutoLayout() {
+        translatesAutoresizingMaskIntoConstraints = !translatesAutoresizingMaskIntoConstraints
+    }
+    
+    var top: NSLayoutYAxisAnchor {
+        return safeAreaLayoutGuide.topAnchor
+    }
+    
+    var bottom: NSLayoutYAxisAnchor {
+        return safeAreaLayoutGuide.bottomAnchor
+    }
+    
+    var right: NSLayoutXAxisAnchor {
+        return safeAreaLayoutGuide.leadingAnchor
+    }
+    
+    var left: NSLayoutXAxisAnchor {
+        return safeAreaLayoutGuide.trailingAnchor
+    }
+    
+    var centerX: NSLayoutXAxisAnchor {
+        return safeAreaLayoutGuide.centerXAnchor
+    }
+    
+    var centerY: NSLayoutYAxisAnchor {
+        return safeAreaLayoutGuide.centerYAnchor
+    }
+    
+    var width: NSLayoutDimension {
+        return safeAreaLayoutGuide.widthAnchor
+    }
+    
+    var height: NSLayoutDimension {
+        return safeAreaLayoutGuide.heightAnchor
+    }
+}
