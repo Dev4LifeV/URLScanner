27a272eafc62ba248714ad260facd55cff9a784e
Victor <vctudo.27@gmail.com>
2023-02-01 14:44:20 -0300

    ✨ create service for API requests

---
+++ Core/Service/ServiceAPIRequest.swift
@@ -0,0 +1,18 @@
+//
+//  ServiceAPIRequest.swift
+//  URLScanner
+//
+//  Created by Victor Proppe on 26/01/23.
+//
+
+import Foundation
+import Combine
+
+protocol ServiceAPIRequest {
+    associatedtype T: Decodable
+    
+    var decoder: JSONDecoder { get }
+    var request: URLRequest { get }
+    
+    func sendRequest(with completion: @escaping (Result<T, Error>) -> Void)
+}
