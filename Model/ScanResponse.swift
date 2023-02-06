0761b75ef1391f7d89866d3b5d50de5fabb19f6e
Victor <vctudo.27@gmail.com>
2023-02-06 10:05:03 -0300

    🏗️ adjusted model for nil safety

--- Model/ScanResponse.swift
+++ Model/ScanResponse.swift
@@ -8,31 +8,44 @@
 import Foundation
 
 struct ScanResponse {
-    let message: String
-    let success: Bool
-    let unsafe: Bool
-    let domain: String
-    let ipAddress: String
-    let server: String
-    let contentType: String
-    let statusCode: Int
-    let pageSize: Int
-    let domainRank: Int
-    let dnsValid: Bool
-    let parking: Bool
-    let spamming: Bool
-    let malware: Bool
-    let phishing: Bool
-    let suspicious: Bool
-    let adult: Bool
-    let riskScore: Int
-    let domainAge: DomainAge
-    let category: String
-    let requestId: String
-}
-
-struct DomainAge {
-    let human: String
-    let timeStamp: Int
-    let iso: String
+    var message: String = ""
+    var success: Bool = false
+    var unsafe: Bool = false
+    var domain: String = ""
+    var ipAddress: String = ""
+    var server: String = ""
+    var contentType: String = ""
+    var statusCode: Int = 0
+    var pageSize: Int = 0
+    var domainRank: Int = 0
+    var dnsValid: Bool = false
+    var parking: Bool = false
+    var spamming: Bool = false
+    var malware: Bool = false
+    var phishing: Bool = false
+    var suspicious: Bool = false
+    var adult: Bool = false
+    var riskScore: Int = 0
+    var category: String = ""
+    var requestId: String = ""
+    
+    var asDictionary : [String: Any] {
+        let mirror = Mirror(reflecting: self)
+        
+        let dict = Dictionary(
+            uniqueKeysWithValues: mirror.children.lazy.map(
+                {
+                    (label: String?, value: Any) -> (String, Any)? in
+                    
+                    guard let label = label else {
+                        return nil
+                    }
+                    
+                    return (label, value)
+                }
+            ).compactMap { $0 })
+        
+        return dict
+        
+    }
 }
