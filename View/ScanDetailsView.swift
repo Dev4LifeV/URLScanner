8f025da31f4633b5cdf97a7d9137b277be920dda
Victor <vctudo.27@gmail.com>
2023-02-06 10:09:18 -0300

    🚧 / 💄 adjusting table

--- View/ScanDetailsView.swift
+++ View/ScanDetailsView.swift
@@ -13,9 +13,10 @@ class ScanDetailsView: UITableView, UITableViewDelegate, UITableViewDataSource {
         
         self.delegate = self
         self.dataSource = self
-        
         self.isScrollEnabled = false
         self.register(ScanDetailsCell.self, forCellReuseIdentifier: "cell")
+        self.backgroundColor = .gray
+        self.layer.cornerRadius = 10
     }
     
     required init?(coder: NSCoder) {
@@ -23,18 +24,20 @@ class ScanDetailsView: UITableView, UITableViewDelegate, UITableViewDataSource {
     }
     
     let mockData =
-        ScanResponse(message: "", success: true, unsafe: true, domain: "domain", ipAddress: "", server: "", contentType: "", statusCode: 0, pageSize: 0, domainRank: 0, dnsValid: true, parking: true, spamming: true, malware: true, phishing: true, suspicious: true, adult: true, riskScore: 0, domainAge: DomainAge(human: "", timeStamp: 0, iso: ""), category: "", requestId: "")
+        ScanResponse(message: "", success: true, unsafe: true, domain: "domain", ipAddress: "", server: "", contentType: "", statusCode: 0, pageSize: 0, domainRank: 0, dnsValid: true, parking: true, spamming: true, malware: true, phishing: true, suspicious: true, adult: true, riskScore: 0, category: "", requestId: "")
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 60
+        return Mirror(reflecting: mockData).children.count
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell", for: indexPath) as! ScanDetailsCell
 
-        cell.scanDetail = mockData
+        cell.key = "\(Array(mockData.asDictionary.keys)[indexPath.row]): ".capitalized
+        cell.value = "\(Array(mockData.asDictionary.values)[indexPath.row])"
         
         return cell
+    
     }
     
     func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
@@ -48,41 +51,41 @@ class ScanDetailsView: UITableView, UITableViewDelegate, UITableViewDataSource {
 
 internal class ScanDetailsCell: UITableViewCell {
     
-    var scanDetail: ScanResponse? {
-        didSet {
-            
-            detail.text = self.scanDetail?.domain ?? "AAA"
-            detail.textColor = .black
-            
-            status.text = String(self.scanDetail?.statusCode ?? 0)
-            detail.textColor = .black
-        }
-    }
-    
-    let detail = UILabel()
-    let status = UILabel()
-    
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
         
-        let info = UIView(frame: CGRect(x: 0, y: 0, width: frame.width - 30, height: 0))
-        
-        detail.enableAutoLayout()
-        status.enableAutoLayout()
-        info.enableAutoLayout()
-        
-        info.addSubview(status)
-        info.addSubview(detail)
-        
-        [
-            status.left.constraint(equalTo: detail.right, constant: info.frame.width)
-        ].activate()
-        
-        contentView.addSubview(info)
+        self.backgroundColor = .clear
+        self.keyLabel.textColor = .white
+        self.valueLabel.textColor = .white
     }
-
     
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
+    
+    private let keyLabel = UILabel()
+    
+    var key: String = "" {
+        didSet {
+            keyLabel.text = self.key
+            keyLabel.enableAutoLayout()
+            contentView.addSubview(keyLabel)
+            keyLabel.height.constraint(equalToConstant: 40).isActive = true
+            keyLabel.left.constraint(equalTo: self.left, constant: 20).isActive = true
+        }
+    }
+    
+    private let valueLabel = UILabel()
+    
+    var value: String = "" {
+        didSet {
+            valueLabel.enableAutoLayout()
+            valueLabel.text = self.value
+            valueLabel.height.constraint(equalToConstant: 40).isActive = true
+            contentView.addSubview(valueLabel)
+            valueLabel.left.constraint(equalTo: keyLabel.right).isActive = true
+        }
+        
+    }
+    
 }
